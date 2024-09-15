b0a92fa40dc35ba7a4702bf3b29319a475df65dd
0xM1nt <minthitkhine2005@gmail.com>
2024-09-15 20:47:04 +0800

    Refactor adminController.js and add user management functionality

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -19,3 +19,61 @@ exports.deleteBucket = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.createUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).send("Username and password are required");
+    } else if (password.length < 8 || password.length > 40) {
+      return res.status(400).send("Password must be between 8 and 40 characters");
+    }
+
+    const result = await minioService.createUser(username, password);
+
+    res.status(201).json({ message: "User created successfully", result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+
+    if (!username) {
+      return res.status(400).send("Username is required");
+    }
+
+    const result = await minioService.deleteUser(username);
+
+    res.status(200).json({ message: "User deleted successfully", result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.listUsers = async (req, res, next) => {
+  try {
+    const users = await minioService.listUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.setUserPolicy = async (req, res, next) => {
+  try {
+    const { username, bucketName } = req.body;
+
+    if (!username || !bucketName) {
+      return res.status(400).send("Username and bucket name are required");
+    }
+
+    const result = await minioService.setUserPolicy(username, bucketName);
+    res.status(200).json({ message: "User policy set successfully", result });
+  } catch (error) {
+    next(error);
+  }
+};
