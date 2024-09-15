efe6acd4743008c8d172289172e782e122c0bc2b
0xM1nt <minthitkhine2005@gmail.com>
2024-09-15 15:29:38 +0800

    Add minioConfig.js file for configuring MinIO connection

---
+++ config/minioConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  endPoint: process.env.MINIO_ENDPOINT || "localhost",
+  port: parseInt(process.env.MINIO_PORT) || 9000,
+  useSSL: false,
+  accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
+  secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
+  bucketName: "my-bucket",
+};
