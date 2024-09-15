d7bfd0b83542adfc2ff2f04996cca1007e947028
0xM1nt <minthitkhine2005@gmail.com>
2024-09-15 20:46:28 +0800

    Refactor minioConfig.js to use environment variables for configuration

--- config/minioConfig.js
+++ config/minioConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   endPoint: process.env.MINIO_ENDPOINT || "localhost",
-  port: parseInt(process.env.MINIO_PORT) || 9000,
+  port: parseInt(process.env.MINIO_PORT),
   useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
-  secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
-  bucketName: "my-bucket",
+  accessKey: process.env.MINIO_ACCESS_KEY,
+  secretKey: process.env.MINIO_SECRET_KEY_FILE,
+  alias: process.env.MINIO_ALIAS,
 };
