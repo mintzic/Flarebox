09ae87ef548c157fe8d71777d217deda86a24260
0xM1nt <minthitkhine2005@gmail.com>
2024-09-15 15:29:51 +0800

    Add admin routes for creating and deleting buckets

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const adminController = require("../controllers/adminController");
+
+const router = express.Router();
+
+router.post("/bucket", adminController.createBucket);
+router.delete("/bucket/:bucketName", adminController.deleteBucket);
+// router.post("/user", adminController.createUser);
+// router.delete("/user/:username", adminController.deleteUser);
+
+module.exports = router;
