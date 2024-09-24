2944a4a9c30d8cf0dbb536ae704cda53dcda23a9
0xM1nt <minthitkhine2005@gmail.com>
2024-09-24 14:20:17 +0800

    Refactor minioConfig.js to use environment variable for secret key

--- config/minioConfig.js
+++ config/minioConfig.js
@@ -1,10 +1,8 @@
-const fs = require("fs");
-
 module.exports = {
   endPoint: process.env.MINIO_ENDPOINT || "localhost",
   port: parseInt(process.env.MINIO_PORT),
   useSSL: false,
   accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: fs.readFileSync(process.env.MINIO_SECRET_KEY_FILE).toString(),
+  secretKey: process.env.MINIO_SECRET_KEY,
   alias: process.env.MINIO_ALIAS,
 };
